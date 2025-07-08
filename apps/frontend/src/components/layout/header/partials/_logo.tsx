6adb0c28dbe47a8a74600d43c5ee20dc0afdd652
Chris Vafiadis <chrisvaf@gmail.com>
2025-07-08 13:58:51 -0400

    Added trasparent logo.

--- apps/frontend/src/components/layout/header/partials/_logo.tsx
+++ apps/frontend/src/components/layout/header/partials/_logo.tsx
@@ -10,7 +10,7 @@ type LogoProps = JSX.IntrinsicElements["a"] & {
 };
 
 export const Logo: FunctionComponent<LogoProps> = ({
-  logo = "/assets/cohnreznick-logo.png",
+  logo = "/assets/cohnreznick-logo-transparent.png",
   ...divProps
 }) => {
   // Create a stable default value that doesn't change every render
